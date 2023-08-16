d177bfb879bec9d1a8a1c1ee195b5188dd554fe0
MiguelZea12 <aroryt02@gmail.com>
2023-08-16 16:12:17 -0500

    First commint

---
+++ core/pos/choices.py
@@ -0,0 +1,17 @@
+genders = (
+    ('male','Masculino'),
+    ('female','Femenino'),
+)
+
+opciones = (
+    ('Yes', 'Si'),
+    ('No', 'No')
+)
+
+identificacion = (
+    ('identification card, Cedula', 'Cedula'),
+    ('Passport', 'Pasaporte'),
+    ('Final consumer', 'Consumidor final'),
+    ('foreign identification',  'Identifacion exterior')
+    
+)
